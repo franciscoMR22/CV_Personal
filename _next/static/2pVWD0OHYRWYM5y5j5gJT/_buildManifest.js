2cfa3fda11d6442b4df0a25d057dbeff193f2fc5
CopperyCuppid54 <franciscomirelesrodriguez22@gmail.com>
2026-03-08 03:46:33 -0600

    Updates

--- _next/static/2pVWD0OHYRWYM5y5j5gJT/_buildManifest.js
+++ _next/static/2pVWD0OHYRWYM5y5j5gJT/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/CV_Personal//_next/:path+",
+        "destination": "/CV_Personal/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
